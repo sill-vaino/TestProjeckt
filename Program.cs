0e98f6a850f034a0489f24abaa4e18ab1230bc4b
Silver Vaino <Sill.vaino@gmail.com>
2024-08-25 15:52:38 +0300

    Read data from input

--- Program.cs
+++ Program.cs
@@ -1,19 +1,41 @@
-﻿namespace BusDriverBreaks
+﻿using System.IO;
+
+namespace BusDriverBreaks
 {
     class Program
     {
         static void DataFromFile()
         {
-            Console.WriteLine("Data From file Acticated!");
+            
         }
 
         static void DataFromInput()
         {
-            Console.WriteLine("Data From Input Activated!");
+            List<string> breakTimes = new List<string>();
+            Console.WriteLine("Add new line as format HH:mmHH:mm");
+            string breakTime = Console.ReadLine();
+            while (true)
+            {
+                if (breakTime == "q")
+                {
+                    break;
+                }
+                else
+                {
+                    breakTimes.Add(breakTime);
+                    Console.WriteLine("Add another line as format HH:mmHH:mm or quit the proccess by entering 'q'");
+                    breakTime = Console.ReadLine();
+                }
+            }
+                foreach (string time in breakTimes)
+                {
+                    Console.WriteLine(time);
+                }
         }
         static void Main(string[] args) {
-            Console.WriteLine("Insertage (i/I) as if you want to insert data by hand or (f/F) if you want data from file");
+            Console.WriteLine("Insert (i/I) as if you want to insert data by hand or (f/F) if you want data from file");
             string Choice = Console.ReadLine();
+
             
 
             while (true)
@@ -24,8 +46,7 @@
                     break;
                 }
                 else if (Choice.ToUpper() == "F")
-                {
-                    DataFromFile();
+                { DataFromFile();
                     break;
                 }
                 else
